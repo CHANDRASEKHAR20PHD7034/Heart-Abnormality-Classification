0bae26095aaab1dfa137b3a79ed4f2214635d27c
CHANDRASEKHAR20PHD7034 <89379330+CHANDRASEKHAR20PHD7034@users.noreply.github.com>
2024-12-25 15:01:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,96 @@
+#Existing PAM
+pamCacc=""
+pamCtime=""
+
+#Existing FCM
+fcmCacc=""
+fcmCtime=""
+
+#Existing KMeans
+kmeansCacc=""
+kmeansCtime = ""
+
+#Existing FFC
+ffcCacc=""
+ffcCtime = ""
+
+#Proposed RFFC
+rffcCacc = ""
+rffcCtime =""
+
+#Existing DNN
+ednnpre=""
+ednnrec=""
+ednnfsc=""
+ednnacc=""
+ednnsens=""
+ednnspec=""
+ednnct=""
+ednnmcc = ""
+ednnfpr=""
+ednnfnr=""
+
+
+#Existing ANN
+eannpre=""
+eannrec=""
+eannfsc=""
+eannacc=""
+eannsens=""
+eannspec=""
+eannct=""
+eannmcc = ""
+eannfpr=""
+eannfnr=""
+
+
+#Existing DJRNN
+edjrnnpre=""
+edjrnnrec=""
+edjrnnfsc=""
+edjrnnacc=""
+edjrnnsens=""
+edjrnnspec=""
+edjrnnct=""
+edjrnnmcc = ""
+edjrnnfpr=""
+edjrnnfnr=""
+
+
+#Existing RNN
+ernnpre=""
+ernnrec=""
+ernnfsc=""
+ernnacc=""
+ernnsens=""
+ernnspec=""
+ernnct=""
+ernnmcc = ""
+ernnfpr=""
+ernnfnr=""
+
+
+#Existing ENN
+eennpre=""
+eennrec=""
+eennfsc=""
+eennacc=""
+eennsens=""
+eennspec=""
+eennct=""
+eennmcc = ""
+eennfpr=""
+eennfnr=""
+
+
+#Proposed PJM-DJRNN
+ppjmdjrnnpre=""
+ppjmdjrnnrec=""
+ppjmdjrnnfsc=""
+ppjmdjrnnacc=""
+ppjmdjrnnsens=""
+ppjmdjrnnspec=""
+ppjmdjrnnct=""
+ppjmdjrnnmcc = ""
+ppjmdjrnnfpr=""
+ppjmdjrnnfnr=""
