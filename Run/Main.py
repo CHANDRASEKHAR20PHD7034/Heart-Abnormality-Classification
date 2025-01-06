266f0fad6982ad4a30680fd702490c528cd91fb5
NADIKATLA CHANDRASEKHAR <89379330+CHANDRASEKHAR20PHD7034@users.noreply.github.com>
2025-01-06 07:43:31 +0530

    Add files via upload

---
+++ Run/Main.py
@@ -0,0 +1,8 @@
+print("HEART DISEASE PREDICTION WITH SEVERITY CLASSIFICATION USING ECG AND PCG SIGNALS BY NOVEL PJM-DJRNN ALGORITHM")
+print("============================================================================================================")
+print("Start the process")
+import warnings
+warnings.filterwarnings('ignore')
+from GUI import Main_gui
+from Code import Heart_disease_prediction_Using_PJM_DJRNN_Training
+from Code import Heart_disease_prediction_Using_PJM_DJRNN_Testing
